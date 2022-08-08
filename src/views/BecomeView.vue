73b79c4fd81311208706c03ac87b094a2cca1850
EsraKlemann <98453430+EsraKlemann@users.noreply.github.com>
2022-08-08 15:22:22 +0200

    Changed text color

--- src/views/BecomeView.vue
+++ src/views/BecomeView.vue
@@ -1,14 +1,14 @@
 <template>
 
   <section id="hero">
-    <!-- Flex container -->
+    <!-- FLEX CONTAINER -->
     <div class="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 lg:flex-row">
       <!-- left item -->
       <div class="flex flex-col mb-32 space-y-1 lg:w-1/2">
         <h1 class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left pt-16">
           Ontdek jouw deal
         </h1>
-        <p class="max-w-sm text-center text-slate-600 md:text-left pb-2">
+        <p class="max-w-sm text-center text-slate-300 sm:text-slate-600 md:text-left pb-2">
           In minder dan een minuut
         </p>
         <!-- Form 1 -->
