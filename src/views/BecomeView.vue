ed8076aea9909170b8eb7063d706ebfc09542408
EsraKlemann <98453430+EsraKlemann@users.noreply.github.com>
2022-08-08 16:44:32 +0200

    More clearly what is where

--- src/views/BecomeView.vue
+++ src/views/BecomeView.vue
@@ -3,23 +3,24 @@
   <section id="hero">
     <!-- FLEX CONTAINER -->
     <div class="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 lg:flex-row">
-      <!-- left item -->
-      <div class="flex flex-col mb-32 space-y-1 lg:w-1/2">
+      <!-- LEFT FORM -->
+      <div class="flex flex-col mb-32 space-y-1 lg:w-2/3">
         <h1 class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left pt-16">
           Ontdek jouw deal
         </h1>
         <p class="max-w-sm text-center text-slate-300 sm:text-slate-600 md:text-left pb-2">
           In minder dan een minuut
         </p>
-        <!-- Form 1 -->
+        <!-- FORM 1 -->
         <div class="flex justify-center md:justify-start w-fit">
           <FormOne />
         </div>
+        <!-- FORM 2 -->
         <div class="flex justify-center">
           <FormTwo />
         </div>
       </div>
-      <!-- image -->
+      <!-- RIGHT IMAGE -->
       <div class="sm:w-auto w-1/2 mx-auto flex flex-shrink justify-end pl-30 md:justify-end">
         <img :src="dezelfde" alt="Dezelfde energie gewoon goedkoper" />
       </div>
