8e2e2f5152df5df3c7981b72970f17369a8e6748
EsraKlemann <98453430+EsraKlemann@users.noreply.github.com>
2022-08-08 16:43:33 +0200

    Added cards to show horizontally on small device

--- src/components/become-form/FormTwo.vue
+++ src/components/become-form/FormTwo.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="bg-gray-100 flex row-auto">
-    <CardEnergyType @click="electraSelection" title="Stroom" :class="{ 'bg-sky-800': selected === 'Stroom' }" ></CardEnergyType>
-    <CardEnergyType @click="electraGasSelection" :class="{ 'bg-sky-800': selected === 'Stroom & Gas' }" title="Stroom & Gas"></CardEnergyType>
+  <!-- CONTAINER HEADING AND ENERGY BLOCKS -->
+  <div class="max-w-7xl px-5 mx-auto mt-32 text-center">
+    <!-- HEADING -->
+    <h2 class="text-3xl font-bold text-center">
+      Kies uw energie
+    </h2>
+    <!-- CONTAINER CARDS -->
+    <div class="flex flex-col bg-slate-50 mt-3 px-2 sm:flex-row sm:space-x-2 ">
+      <!-- ENERGY CARDS -->
+      <CardEnergyType @click="gasSelection" title="Gas"
+        :class="{ 'bg-sky-800 transition ease-in-out translate-y-0 duration-200': selected === 'Gas' }" />
+      <CardEnergyType @click="electraGasSelection" title="Stroom & Gas"
+        :class="{ 'bg-sky-800': selected === 'Stroom & Gas' }" />
+      <CardEnergyType @click="electraSelection" title="Stroom" :class="{ 'bg-sky-800': selected === 'Stroom' }" />
+    </div>
   </div>
 </template>
 
@@ -14,6 +26,8 @@ export default {
   data() {
     return {
       selected: '',
+      gasFormVisible: false,
+
     }
   },
   methods: {
@@ -23,6 +37,33 @@ export default {
     electraGasSelection() {
       this.selected = 'Stroom & Gas'
     },
+    gasSelection() {
+      this.selected = 'Gas'
+    },
+    async getContract() {
+      const response = await fetch('https://mega-become-test-default-rtdb.europe-west1.firebasedatabase.app/products.json');
+
+      const responseData = await response.json();
+      if (response.ok) {
+        // ga naar volgende form stap. router beweeg naar invisible item? 
+      } else {
+        throw new Error('Could not save data!');
+      }
+
+      const contracts = [];
+
+      for (const key in responseData) {
+        const contract = {
+          name: responseData[key].code,
+          contractType: responseData[key].contract_type,
+          duration: responseData[key].duration,
+          energyType: responseData[key].energy_type,
+          id: key,
+          descriptions: responseData[key].descriptions,
+        }
+        contracts.push(contract);
+      }
+    }
   }
 }
 
--- src/components/become-form/cards/CardEnergyType.vue
+++ src/components/become-form/cards/CardEnergyType.vue
@@ -1,16 +1,21 @@
 <template>
-  <div  class="max-w-md rounded overflow-hidden shadow-lg bg-white">
-    <img :src="require(`@/assets/energy-icons/${title}.png`)" class="h-40" alt="test">
-    <div class="px-6 py-4">
-      <div class="font-bold text-xl mb-2">{{title}}</div>
+
+  <!-- WHITE OUTSIDE BORDER -->
+  <div class="flex sm:flex-col items-center p-1 space-y-6 rounded md:w-1/3 overflow-hidden shadow-lg bg-white">
+    <!-- IMAGE -->
+    <img :src="require(`@/assets/energy-icons/${title}.png`)" class="w-20 sm:w-40 p-3" alt="image of energy type">
+    <!-- TITLE TEXT -->
+    <div class="px-6 h-20 text-center">
+      <div class="font-bold sm:text-2xl mb-2 ">{{ title }}</div>
     </div>
-    <div class="px-6 pt-4 pb-2">
-      
-      <span
-        class="inline-block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-        >Bekijk prijzen -></span>
-    </div>
+    <!-- BUTTON TO CONTINUE -->
+    <!-- <div class=" pt-4 pb-2">
+      <span class="inline-block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        -></span>
+    </div> -->
   </div>
+
+
 </template>
 
 <script>
@@ -21,31 +26,18 @@ export default {
       type: String,
       required: true
     },
+    selected: {
+      type: String,
+      required: false
+    }
   },
   data() {
     return {
+
     }
   },
   methods: {
-    getContract() {
-      const response = await fetch('https://mega-become-test-default-rtdb.europe-west1.firebasedatabase.app/products.json');
 
-      const responseData = await response.json();
-      if (response.ok) {
-        // ga naar volgende form stap. router beweeg naar invisible item? 
-      } else {
-        throw new Error('Could not save data!');
-      }
-
-      const contracts = [];
-
-      for (const key in responseData) {
-        const contract = {
-          
-        }
-
-      }
-    }
   }
 }
 </script>
