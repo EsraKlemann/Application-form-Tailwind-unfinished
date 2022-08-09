d61bdf4c44aaf7f65c304c17f08422a1d6d7e93b
EsraKlemann <98453430+EsraKlemann@users.noreply.github.com>
2022-08-09 14:02:40 +0200

    Small padding change

--- src/views/BecomeView.vue
+++ src/views/BecomeView.vue
@@ -5,7 +5,7 @@
     <div class="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 lg:flex-row">
       <!-- LEFT FORM -->
       <div class="flex flex-col mb-32 space-y-1 lg:w-2/3">
-        <h1 class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left pt-16">
+        <h1 class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left pt-20">
           Ontdek jouw deal
         </h1>
         <p class="max-w-sm text-center text-slate-300 sm:text-slate-600 md:text-left pb-2">
